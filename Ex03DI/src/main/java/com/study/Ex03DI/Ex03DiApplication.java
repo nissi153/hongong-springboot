9338555eb4ae245f4a388da8fdacbc0c61fc5e49
nissi153 <nissisoft21@gmail.com>
2022-10-14 17:04:48 +0900

    Ex03DI

---
+++ Ex03DI/src/main/java/com/study/Ex03DI/Ex03DiApplication.java
@@ -0,0 +1,13 @@
+package com.study.Ex03DI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ex03DiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ex03DiApplication.class, args);
+	}
+
+}
---
+++ Ex03DI/src/test/java/com/study/Ex03DI/Ex03DiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.Ex03DI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ex03DiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
