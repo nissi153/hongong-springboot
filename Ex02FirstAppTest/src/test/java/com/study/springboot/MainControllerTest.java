ab72f88119b761c52b28247a967e975257298526
nissi153 <nissisoft21@gmail.com>
2022-10-14 16:17:34 +0900

    Ex02FirstAppTest

--- Ex02FirstAppTest/src/test/java/com/study/springboot/MainControllerTest.java
+++ Ex02FirstAppTest/src/test/java/com/study/springboot/MainControllerTest.java
@@ -1,14 +1,16 @@
 package com.study.springboot;
 
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+@ExtendWith(SpringExtension.class)
 @WebMvcTest(controllers = MainController.class)
 class MainControllerTest {
 
