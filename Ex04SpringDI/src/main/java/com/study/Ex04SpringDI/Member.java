2fe756483271f06e89e6dc52314f38e06ee7b96c
nissi153 <nissisoft21@gmail.com>
2022-10-17 12:39:32 +0900

    Ex04SpringDI

---
+++ Ex04SpringDI/src/main/java/com/study/Ex04SpringDI/Member.java
@@ -0,0 +1,28 @@
+package com.study.Ex04SpringDI;
+
+//회원정보 클래스
+public class Member {
+    private String name;
+
+    public Member(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    //싱글톤 만들기
+    private static Member m = null;
+    public static Member getInstance() {
+        if( m == null ) {
+            m = new Member("홍길동");
+        }
+        return m;
+    }
+
+}
