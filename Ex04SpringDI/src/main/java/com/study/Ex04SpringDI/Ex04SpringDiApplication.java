7f4ac1273d7ffce971a09b4189249426910f69b0
nissi153 <nissisoft21@gmail.com>
2022-10-17 12:38:33 +0900

    Ex04SpringDI

---
+++ Ex04SpringDI/src/main/java/com/study/Ex04SpringDI/Ex04SpringDiApplication.java
@@ -0,0 +1,13 @@
+package com.study.Ex04SpringDI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ex04SpringDiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ex04SpringDiApplication.class, args);
+	}
+
+}
