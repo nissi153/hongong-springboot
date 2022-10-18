9ef69fa04fa3e3104365ad5bbb6e1ca7b2d80f32
nissi153 <nissisoft21@gmail.com>
2022-10-18 17:45:36 +0900

    Ex04DIType

--- Ex05DIType/src/main/java/com/study/springboot/Member.java
+++ Ex05DIType/src/main/java/com/study/springboot/Member.java
@@ -6,17 +6,19 @@ import org.springframework.stereotype.Component;
 public class Member {
     private String name = "사임당";
 
+    //기본생성자
+    //기본생성자 안 만들어주면 빈 등록이 안됨.
     public Member() {
     }
-
+    //필드가 있는 생성자
     public Member(String name) {
         this.name = name;
     }
-
+    //getter
     public String getName() {
         return name;
     }
-
+    //setter
     public void setName(String name) {
         this.name = name;
     }
