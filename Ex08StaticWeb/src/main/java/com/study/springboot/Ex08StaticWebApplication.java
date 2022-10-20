44e80ac3567dec364f696446477046f97295624c
nissi153 <nissisoft21@gmail.com>
2022-10-20 17:16:59 +0900

    Ex08StaticWeb

---
+++ Ex08StaticWeb/src/main/java/com/study/springboot/Ex08StaticWebApplication.java
@@ -0,0 +1,13 @@
+package com.study.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ex08StaticWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ex08StaticWebApplication.class, args);
+	}
+
+}
