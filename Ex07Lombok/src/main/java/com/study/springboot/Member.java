302074d09ca09caca682633f22f1172a15a92174
nissi153 <nissisoft21@gmail.com>
2022-10-20 12:05:34 +0900

    Ex07Lombok

---
+++ Ex07Lombok/src/main/java/com/study/springboot/Member.java
@@ -0,0 +1,11 @@
+package com.study.springboot;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Member {
+    //사용자 아이디
+    private String username;
+    //사용자 비밀번호
+    private String password;
+}
