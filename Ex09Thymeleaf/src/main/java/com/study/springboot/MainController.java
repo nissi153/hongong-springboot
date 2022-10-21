60e859e71bc9b66e31cb8266ca5a0152e4b8abd8
nissi153 <nissisoft21@gmail.com>
2022-10-21 16:50:27 +0900

    Ex09Thymeleaf

--- Ex09Thymeleaf/src/main/java/com/study/springboot/MainController.java
+++ Ex09Thymeleaf/src/main/java/com/study/springboot/MainController.java
@@ -4,6 +4,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Date;
+
 @Controller
 public class MainController {
 
@@ -16,11 +20,30 @@ public class MainController {
 	public String index1(Model model) {
 		
 		model.addAttribute("name", "홍길동");
-		model.addAttribute("name_h1", "<h2 style='color:red;'>홍길동</h2>");
+		model.addAttribute("name_html", "<h2 style='color:red;'>홍길동</h2>");
 		model.addAttribute("value", "서버값");
 		model.addAttribute("var", "변수값");
 
 		return "index1"; //index1.html로 응답함.
 	}
 
+	@GetMapping("/index2")
+	public String index2(Model model) {
+
+		model.addAttribute("address", "한양");
+		model.addAttribute("address_null", null);
+		model.addAttribute("address_empty", "");
+
+		return "index2"; //index2.html로 응답함.
+	}
+
+	@GetMapping("/index3")
+	public String index3(Model model) {
+
+		model.addAttribute( "standardDate", new Date() );
+		model.addAttribute( "localDate", LocalDate.now() );
+		model.addAttribute( "localDateTime", LocalDateTime.now() );
+
+		return "index3"; //index3.html로 응답함.
+	}
 }
