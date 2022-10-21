d0d1b41c3419629fbfc3d2d3e68d54bea17ac23f
nissi153 <nissisoft21@gmail.com>
2022-10-21 17:56:02 +0900

    Ex09Thymeleaf

--- Ex09Thymeleaf/src/main/java/com/study/springboot/MainController.java
+++ Ex09Thymeleaf/src/main/java/com/study/springboot/MainController.java
@@ -6,7 +6,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 @Controller
 public class MainController {
@@ -44,6 +46,34 @@ public class MainController {
 		model.addAttribute( "localDate", LocalDate.now() );
 		model.addAttribute( "localDateTime", LocalDateTime.now() );
 
+		model.addAttribute( "number1", 12345678 );
+		model.addAttribute( "number2", 123456.789 );
+
 		return "index3"; //index3.html로 응답함.
+	}
+
+	@GetMapping("/index4")
+	public String index4(Model model) {
+
+		model.addAttribute("web", "web");
+
+		model.addAttribute("role", "admin");
+
+		return "index4"; //index4.html로 응답함.
+	}
+
+	@GetMapping("index5")
+	public String index5(Model model) {
+
+		Member member = new Member("hong", "1234");
+		model.addAttribute("member", member);
+
+		List<Member> list = new ArrayList<Member>();
+		list.add( new Member("lee", "2222")  );
+		list.add( new Member("hana", "3333")  );
+		list.add( new Member("tom", "4444")  );
+		model.addAttribute("list", list);
+
+		return "index5"; //index5.html로 응답함.
 	}
 }
