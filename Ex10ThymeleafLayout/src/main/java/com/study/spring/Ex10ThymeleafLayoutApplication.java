848f3463b1e66f406d47355fad9323b56555003c
nissi153 <nissisoft21@gmail.com>
2022-10-25 14:07:21 +0900

    Ex10ThymeleafLayout

---
+++ Ex10ThymeleafLayout/src/main/java/com/study/spring/Ex10ThymeleafLayoutApplication.java
@@ -0,0 +1,13 @@
+package com.study.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ex10ThymeleafLayoutApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ex10ThymeleafLayoutApplication.class, args);
+	}
+
+}
